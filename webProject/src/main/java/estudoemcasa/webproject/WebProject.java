56e71381b65652ea96295358f10cf85be937ee48
Ketlin Oliveira <ketlinkii2004@gmail.com>
2025-12-22 15:13:16 -0300

    Project created

---
+++ webProject/src/main/java/estudoemcasa/webproject/WebProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package estudoemcasa.webproject;
+
+/**
+ *
+ * @author ketli
+ */
+public class WebProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
